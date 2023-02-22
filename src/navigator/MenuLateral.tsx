f35a04502b6888400d94d853b9089601f077eb66
Eric Hernandez <erichernandez@Erics-MacBook-Pro.local>
2023-02-21 22:10:48 -0600

    Tabs navigation completed

--- src/navigator/MenuLateral.tsx
+++ src/navigator/MenuLateral.tsx
@@ -5,6 +5,7 @@ import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScroll
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreen } from '../screens/SettingsScreen';
 import { styles } from '../theme/appTheme';
+import { Tabs } from './Tabs';
 
 
 export const MenuLateral = () => {
@@ -33,7 +34,7 @@ export const MenuLateral = () => {
             }}
             drawerContent={(props) => <MenuInterno {...props} />}
         >
-            <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+            <Drawer.Screen name="Tabs" component={Tabs} />
             <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
         </Drawer.Navigator>
     )
@@ -55,7 +56,7 @@ const MenuInterno = ( { navigation }: DrawerContentComponentProps) => {
             <View style={ styles.menuContainer }>
                 <TouchableOpacity 
                     style={ styles.menuBoton }
-                    onPress={ ()=> { navigation.navigate('StackNavigator') } }
+                    onPress={ ()=> { navigation.navigate('Tabs') } }
                 >
                     <Text style={ styles.menuTexto }>Navegacion</Text>
                 </TouchableOpacity>
